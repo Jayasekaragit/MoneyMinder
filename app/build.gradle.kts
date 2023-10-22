8e7115be0cffb642ca37b1f229594e667ee3ddcd
Jayasekaragit <pasinduj27@gmail.com>
2023-10-22 13:43:26 +0530

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.moneyminder"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.moneyminder"
--- app/src/main/java/com/example/moneyminder/MainActivity.java
+++ app/src/main/java/com/example/moneyminder/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.moneyminder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String logVariable = "logFile";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(logVariable,"On create");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(logVariable,"onStart");
     }
 }
